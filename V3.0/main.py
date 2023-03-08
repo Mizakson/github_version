2174e99dbdedc75cbe364bee937c3be1b92c7f77
Mizakson <maxwellizakson@gmail.com>
2023-03-08 08:57:35 -0500

    added colorscheme using simple_chalk

--- V3.0/main.py
+++ V3.0/main.py
@@ -25,6 +25,7 @@ User input converted to all uppercase using .upper()
 """
 import sys
 import sqlite3
+from simple_chalk import chalk, red, green, yellow, blue, magenta, cyan, magenta, black
 
 # Connect to database
 conn = sqlite3.connect('media_library.db') # insert your db name here
@@ -78,45 +79,48 @@ def delete_book(title, author):
 # book library main function
 def booklib():
     while True:
-        print("--- LIBRARY MANAGEMENT SYSTEM V3.0 ---")
-        print("1. ADD A BOOK")
-        print("2. VIEW ALL BOOKS")
-        print("3. UPDATE A BOOK'S READ STATUS")
-        print("4. DELETE A BOOK")
-        print("5. RETURN TO MAIN MENU")
-        print("6. QUIT")
-        print("A program by Mizakson")
-        choice = int(input("ENTER OPTION HERE: "))
+        # add simple-chalk library for colored text
+        # change print statements to colored text
+        print(cyan("--- LIBRARY MANAGEMENT SYSTEM V3.0 ---"))
+        print(cyan("1. ADD A BOOK"))
+        print(cyan("2. VIEW ALL BOOKS"))
+        print(cyan("3. UPDATE A BOOK'S READ STATUS"))
+        print(cyan("4. DELETE A BOOK"))
+        print(cyan("5. RETURN TO MAIN MENU"))
+        print(cyan("6. QUIT"))
+        choice = int(input(cyan("ENTER OPTION HERE: ")))
             
         if choice == 1:
-            title = input("ENTER BOOK TITLE: ").upper()
-            author = input("ENTER AUTHOR NAME: ").upper()
-            read = input("HAVE YOU READ THIS BOOK BEFORE? (Y/N): ").upper()
-            genre = input("ENTER GENRE: ").upper()
+            title = input(cyan("ENTER BOOK TITLE: ")).upper()
+            author = input(cyan("ENTER AUTHOR NAME: ")).upper()
+            read = input(cyan("HAVE YOU READ THIS BOOK BEFORE? (Y/N): ")).upper()
+            genre = input(cyan("ENTER GENRE: ")).upper()
             insert_book(title, author, read, genre)
 
         elif choice == 2:
             read_books()
 
         elif choice == 3:
-            title = input("ENTER BOOK TITLE: ").upper()
-            author = input("ENTER AUTHOR NAME: ").upper()
-            read = input("HAVE YOU READ THIS BOOK BEFORE? (Y/N): ").upper()
+            title = input(cyan("ENTER BOOK TITLE: ")).upper()
+            author = input(cyan("ENTER AUTHOR NAME: ")).upper()
+            read = input(cyan("HAVE YOU READ THIS BOOK BEFORE? (Y/N): ")).upper()
             update_book_read(title, author, read)
 
         elif choice == 4:
-            title = input("ENTER BOOK TITLE: ").upper()
-            author = input("ENTER AUTHOR NAME: ").upper()
+            title = input(cyan("ENTER BOOK TITLE: ")).upper()
+            author = input(cyan("ENTER AUTHOR NAME: ")).upper()
             delete_book(title, author)
 
         elif choice == 5:
+            print(green("RETURNING TO MAIN MENU..."))
             break
 
+
         elif choice == 6:
-            sys.exit("PROGRAM TERMINATED")
+            sys.exit(red("PROGRAM TERMINATED"))
 
         else:
-            print("INVALID CHOICE. PLEASE TRY AGAIN.")
+            print(red("INVALID CHOICE. PLEASE TRY AGAIN."))
 
 
 
@@ -137,7 +141,7 @@ c.execute('''CREATE TABLE IF NOT EXISTS songs
 def insert_song(name, artist, genre):
     c.execute("INSERT INTO songs VALUES (?, ?, ?)", (name, artist, genre))
     conn.commit()
-    print("SONG ADDED SUCCESSFULLY.")
+    print(green("SONG ADDED SUCCESSFULLY."))
 
 # Function to read all songs from the database
 def read_songs():
@@ -150,85 +154,90 @@ def read_songs():
 def update_song_genre(name, artist, new_genre):
     c.execute("UPDATE songs SET genre=? WHERE name=? AND artist=?", (new_genre, name, artist))
     conn.commit()
-    print("SONG UPDATED SUCCESSFULLY.")
+    print(green("SONG UPDATED SUCCESSFULLY."))
 
 # Function to delete a song from the database
 def delete_song(name, artist):
     c.execute("DELETE FROM songs WHERE name=? AND artist=?", (name, artist))
     conn.commit()
-    print("SONG DELETED SUCCESSFULLY.")
+    print(green("SONG DELETED SUCCESSFULLY."))
 
 
 def musiclib():  
     
     # Prompt the user for input and call the appropriate CRUD function
     while True:
-        print("--- MUSIC LIBRARY MANAGEMENT SYSTEM V3.0 ---")
-        print("1. ADD A SONG")
-        print("2. VIEW ALL SONGS")
-        print("3. UPDATE A SONG'S GENRE")
-        print("4. DELETE A SONG")
-        print("5. RETURN TO MAIN MENU")
-        print("6. QUIT")
-        print("A program by Mizakson")
-        choice = int(input("ENTER OPTION HERE: "))
+        print(magenta("--- MUSIC LIBRARY MANAGEMENT SYSTEM V3.0 ---"))
+        print(magenta("1. ADD A SONG"))
+        print(magenta("2. VIEW ALL SONGS"))
+        print(magenta("3. UPDATE A SONG'S GENRE"))
+        print(magenta("4. DELETE A SONG"))
+        print(magenta("5. RETURN TO MAIN MENU"))
+        print(magenta("6. QUIT"))
+        choice = int(input(magenta("ENTER OPTION HERE: ")))
 
         if choice == 1:
-            name = input("ENTER SONG NAME: ").upper()
-            artist = input("ENTER ARTIST NAME: ").upper()
-            genre = input("ENTER GENRE: ").upper()
+            name = input(magenta("ENTER SONG NAME: ")).upper()
+            artist = input(magenta("ENTER ARTIST NAME: ")).upper()
+            genre = input(magenta("ENTER GENRE: ")).upper()
             insert_song(name, artist, genre)
-        
+            print(green("SONG ADDED SUCCESSFULLY."))
+
         elif choice == 2:
+            print(green("SONG NAME | ARTIST | GENRE"))
             read_songs()
-        
+            
+
         elif choice == 3:
-            name = input("ENTER SONG NAME: ").upper()
-            artist = input("ENTER ARTIST NAME: ").upper()
-            genre = input("ENTER GENRE: ").upper()
+            name = input(magenta("ENTER SONG NAME: ")).upper()
+            artist = input(magenta("ENTER ARTIST NAME: ")).upper()
+            genre = input(magenta("ENTER GENRE: ")).upper()
             update_song_genre(name, artist, genre)
-        
+            print(green("SONG UPDATED SUCCESSFULLY."))
+
         elif choice == 4:
-            name = input("ENTER SONG NAME: ").upper()
-            artist = input("ENTER ARTIST NAME: ").upper()
+            name = input(magenta("ENTER SONG NAME: ")).upper()
+            artist = input(magenta("ENTER ARTIST NAME: ")).upper()
             delete_song(name, artist)
+            print(green("SONG DELETED SUCCESSFULLY."))
         
         elif choice == 5:
+            print(green("RETURNING TO MAIN MENU..."))
             break
         
         elif choice == 6:
-            sys.exit("PROGRAM TERMINATED")
+            sys.exit(red("PROGRAM TERMINATED"))
         
         else:
-            print("INVALID CHOICE. PLEASE TRY AGAIN.")
+            print(red("INVALID CHOICE. PLEASE TRY AGAIN."))
 
 
 
 # homepage section
 def home_page():
     while True:
-        print("--- PERSONAL MEDIA LIBRARY MANAGEMENT SYSTEM HOMEPAGE V3.0 ---")
-        print("1. GO TO BOOK LIBRARY ")
-        print("2. GO TO MUSIC LIBRARY ")
-        print("3. QUIT ")
-        print("A program by Mizakson")
-        choice = int(input("ENTER OPTION HERE: "))
+        print(blue("--- PERSONAL MEDIA LIBRARY MANAGEMENT SYSTEM HOMEPAGE V3.0 ---"))
+        print(blue("1. GO TO BOOK LIBRARY "))
+        print(blue("2. GO TO MUSIC LIBRARY "))
+        print(blue("3. QUIT "))
+        print(blue("A program by Mizakson"))
+        choice = int(input(blue("ENTER OPTION HERE: ")))
         
         if choice == 1:
-            print("GOING TO BOOK LIBRARY... ")
+            print(cyan("GOING TO BOOK LIBRARY... "))
             booklib()
         
         elif choice == 2:
-            print("GOING TO MUSIC LIBRARY... ")
+            print(magenta("GOING TO MUSIC LIBRARY... "))
             musiclib()
         
         elif choice == 3:
-            print("CLOSING PROGRAM...")
-            print("THANKS FOR VISITING :)")
+            print(red("CLOSING PROGRAM..."))
+            print(red("THANKS FOR VISITING :)"))
             sys.exit()      
         
         else:
-            print("ENTER A VALID OPTION... ")
+            print(red("ENTER A VALID OPTION... "))
 
 
 # initialize homepage function to start the program
